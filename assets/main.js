0dcd76df51a4f6361060a2d426f84248da6eeaac
Arkreuz <coder@arkreuz.com>
2017-01-21 23:40:50 -0600

    Uploaded!

---
+++ assets/main.js
@@ -0,0 +1,161 @@
+var sel = document.getElementById('sel');
+var dis = document.getElementById('display');
+
+
+
+
+
+function loadPKMN(i){
+
+$("#display").append(pokemon[i].icon).hide().fadeIn(1000);
+
+};
+
+
+
+
+
+
+
+$(function() {
+    $("select").change(function() {
+        var val = $(this).val();
+        if (val === "ou") {
+
+            $( "#display" ).empty();
+
+            for (var i = 0; i < pokemon.length; i++) {
+                if (pokemon[i].ou) {
+                     loadPKMN(i);
+                }
+
+            }
+        }
+else if (val === "uu") {
+
+    $( "#display" ).empty();
+    for (var i = 0; i < pokemon.length; i++) {
+        if (pokemon[i].uu) {
+             loadPKMN(i);
+        }
+
+    }
+}
+
+else if (val === "ru") {
+
+    $( "#display" ).empty();
+    for (var i = 0; i < pokemon.length; i++) {
+        if (pokemon[i].ru) {
+             loadPKMN(i);
+        }
+    }
+
+
+}
+
+
+else if (val === "nu") {
+
+    $( "#display" ).empty();
+    for (var i = 0; i < pokemon.length; i++) {
+        if (pokemon[i].nu) {
+             loadPKMN(i);
+        }
+    }
+
+
+}
+
+
+else if (val === "pu") {
+
+    $( "#display" ).empty();
+    for (var i = 0; i < pokemon.length; i++) {
+        if (pokemon[i].pu) {
+             loadPKMN(i);
+        }
+    }
+
+
+}
+
+
+else if (val === "beta") {
+
+    $( "#display" ).empty();
+    for (var i = 0; i < pokemon.length; i++) {
+        if (pokemon[i].beta) {
+             loadPKMN(i);
+        }
+    }
+
+
+}
+
+else if (val === "gamma") {
+
+    $( "#display" ).empty();
+    for (var i = 0; i < pokemon.length; i++) {
+        if (pokemon[i].gamma) {
+             loadPKMN(i);
+        }
+    }
+
+
+}
+
+
+else if (val === "delta") {
+
+    $( "#display" ).empty();
+    for (var i = 0; i < pokemon.length; i++) {
+        if (pokemon[i].delta) {
+             loadPKMN(i);
+        }
+    }
+
+
+}
+
+
+else if (val === "epsilon") {
+
+    $( "#display" ).empty();
+    for (var i = 0; i < pokemon.length; i++) {
+        if (pokemon[i].epsilon) {
+             loadPKMN(i);
+        }
+    }
+
+
+}
+
+else if (val === "zeta") {
+
+    $( "#display" ).empty();
+    for (var i = 0; i < pokemon.length; i++) {
+        if (pokemon[i].zeta) {
+             loadPKMN(i);
+        }
+    }
+
+
+}
+
+    });
+});
+
+
+
+
+
+// for (var i = 0; i < pokemon.length; i++) {
+//     var nimg = document.createElement("img");
+//     nimg.icon = pokemon[i].icon;
+//     document.getElementById("display").append(nimg);
+// }
+
+// nimg.icon = pokemon[1].icon;
+// nimg.icon = pokemon[0].icon;
+// document.getElementById("display").appendChild(nimg);
---
+++ assets/pkdex.js
@@ -0,0 +1,234 @@
+var pokemon = [];
+
+var abomasnow = {
+ "name": "abomasnow",
+    "ou": true,
+    "uu": true,
+    "ru": true,
+    "nu": false,
+    "pu": false,
+    "beta": true,
+    "gamma": true,
+    "delta": true,
+    "epsilon": false,
+    "zeta": false,
+    "icon": "<i class='pk abomasnow'></i>"
+};pokemon.push(abomasnow);
+
+
+
+var abomasnow_mega = {
+    "ou": true,
+    "uu": true,
+    "ru": true,
+    "nu": false,
+    "pu": false,
+    "beta": true,
+    "gamma": true,
+    "delta": true,
+    "epsilon": false,
+    "zeta": false,
+    "icon": "<i class='pk abomasnow-mega'></i>"
+}; pokemon.push(abomasnow_mega);
+
+
+var absol = {
+    "ou": true,
+    "uu": true,
+    "ru": true,
+    "nu": false,
+    "pu": false,
+    "beta": true,
+    "gamma": true,
+    "delta": true,
+    "epsilon":true,
+    "zeta": false,
+    "icon": "<i class='pk absol'></i>"
+}; pokemon.push(absol);
+
+var absol_mega = {
+    "ou": true,
+    "uu": true,
+    "ru": false,
+    "nu": false,
+    "pu": false,
+    "beta": true,
+    "gamma": true,
+    "delta": false,
+    "epsilon": false,
+    "zeta": false,
+    "icon": "<i class='pk absol-mega'></i>"
+}; pokemon.push(absol_mega);
+
+
+var aegislash = {
+    "ou": true,
+    "uu": false,
+    "ru": false,
+    "nu": false,
+    "pu": false,
+    "beta": true,
+    "gamma": false,
+    "delta": false,
+    "epsilon":false,
+    "zeta": false,
+    "icon": "<i class='pk aegislash'></i>"
+}; pokemon.push(aegislash);
+
+
+var aerodactyl = {
+    "ou": true,
+    "uu": true,
+    "ru": true,
+    "nu": false,
+    "pu": false,
+    "beta": true,
+    "gamma": true,
+    "delta": true,
+    "epsilon": false,
+    "zeta": false,
+    "icon": "<i class='pk aerodactyl'></i>"
+}; pokemon.push(aerodactyl);
+
+var aerodactyl_mega = {
+    "ou": true,
+    "uu": true,
+    "ru":false,
+    "nu": false,
+    "pu": false,
+    "beta": true,
+    "gamma": true,
+    "delta": false,
+    "epsilon": false,
+    "zeta": false,
+    "icon": "<i class='pk aerodactyl-mega'></i>"
+}; pokemon.push(aerodactyl_mega);
+
+
+var aggron = {
+    "ou": true,
+    "uu": true,
+    "ru":true,
+    "nu": true,
+    "pu": false,
+    "beta": true,
+    "gamma": true,
+    "delta": true,
+    "epsilon": false,
+    "zeta": false,
+    "icon": "<i class='pk aggron'></i>"
+}; pokemon.push(aggron);
+
+
+var aggron_mega = {
+    "ou": true,
+    "uu": true,
+    "ru":false,
+    "nu": false,
+    "pu": false,
+    "beta": true,
+    "gamma": true,
+    "delta": false,
+    "epsilon": false,
+    "zeta": false,
+    "icon": "<i class='pk aggron-mega'></i>"
+}; pokemon.push(aggron_mega);
+
+var alakazam = {
+    "ou": true,
+    "uu": true,
+    "ru":false,
+    "nu": false,
+    "pu": false,
+    "beta": true,
+    "gamma": true,
+    "delta": false,
+    "epsilon": false,
+    "zeta": false,
+    "icon": "<i class='pk alakazam'></i>"
+}; pokemon.push(alakazam);
+
+
+var alakazam_mega = {
+    "ou": true,
+    "uu": false,
+    "ru":false,
+    "nu": false,
+    "pu": false,
+    "beta": true,
+    "gamma": false,
+    "delta": false,
+    "epsilon": false,
+    "zeta": false,
+    "icon": "<i class='pk alakazam-mega'></i>"
+}; pokemon.push(alakazam_mega);
+
+var alomomola = {
+    "ou": true,
+    "uu": true,
+    "ru": true,
+    "nu": false,
+    "pu": false,
+    "beta": true,
+    "gamma": true,
+    "delta": true,
+    "epsilon": false,
+    "zeta": false,
+    "icon": "<i class='pk alomomola'></i>"
+}; pokemon.push(alomomola);
+
+
+var altaria = {
+    "ou": true,
+    "uu": true,
+    "ru": true,
+    "nu": true,
+    "pu": true,
+    "beta": true,
+    "gamma": true,
+    "delta": true,
+    "epsilon": true,
+    "zeta": false,
+    "icon": "<i class='pk altaria'></i>"
+}; pokemon.push(altaria);
+
+
+var altaria_mega = {
+    "ou": true,
+    "uu": false,
+    "ru": false,
+    "nu": false,
+    "pu": false,
+    "beta": true,
+    "gamma": true,
+    "delta": false,
+    "epsilon": false,
+    "zeta": false,
+    "icon": "<i class='pk altaria-mega'></i>"
+}; pokemon.push(altaria_mega);
+
+
+var ambipom = {
+    "ou": true,
+    "uu": true,
+    "ru": true,
+    "nu": false,
+    "pu": false,
+    "beta": true,
+    "gamma": true,
+    "delta": true,
+    "epsilon": false,
+    "zeta": false,
+    "icon": "<i class='pk ambipom'></i>"
+}; pokemon.push(ambipom);
+
+
+
+
+
+
+
+
+
+
+//Show Pokemon
