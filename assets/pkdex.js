66b145526a571706cd39ece6f9a60867ec6d66f9
Arkreuz <developer@arkreuz.com>
2019-03-15 23:59:58 -0600

    150319 Octi Clean Up

--- assets/pkdex.js
+++ assets/pkdex.js
@@ -4022,12 +4022,7 @@ var omastar ={
         "ru": true,
         "nu": true,
         "pu": false,
-        "alpha": true,
-        "beta": true,
-        "gamma": true,
-        "delta": true,
-        "epsilon": false,
-        "zeta": false,
+     
     },
     "icon": "<i class='pk omastar'></i>"
 }; pokemon.push(omastar);
